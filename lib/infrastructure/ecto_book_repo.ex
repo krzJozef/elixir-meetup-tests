548407ef167de7104d8c255089b5e108554d38ac
Krzysztof Józefowicz <krzysztof.jozefowicz@appunite.com>
2022-06-28 11:43:29 +0200

    Uncomment infrastructure

--- lib/infrastructure/ecto_book_repo.ex
+++ lib/infrastructure/ecto_book_repo.ex
@@ -1,55 +1,55 @@
-# defmodule Bookshelf.Infrastructure.EctoBook do
-#   use Ecto.Schema
+defmodule Bookshelf.Infrastructure.EctoBook do
+  use Ecto.Schema
 
-#   import Ecto.Changeset
+  import Ecto.Changeset
 
-#   alias Bookshelf.Model.Book
+  alias Bookshelf.Model.Book
 
-#   @primary_key {:id, :binary_id, autogenerate: false}
+  @primary_key {:id, :binary_id, autogenerate: false}
 
-#   schema "books" do
-#     field(:title, :string)
-#     field(:isbn, :string)
-#     field(:author_id, :string)
-#     field(:rating, :integer)
+  schema "books" do
+    field(:title, :string)
+    field(:isbn, :string)
+    field(:author_id, :string)
+    field(:rating, :integer)
 
-#     timestamps()
-#   end
+    timestamps()
+  end
 
-#   @fields ~w(id title isbn author_id rating)a
+  @fields ~w(id title isbn author_id rating)a
 
-#   def changeset(%__MODULE__{} = ecto_book, %Book{} = book) do
-#     params = Map.from_struct(book)
+  def changeset(%__MODULE__{} = ecto_book, %Book{} = book) do
+    params = Map.from_struct(book)
 
-#     ecto_book
-#     |> cast(params, @fields)
-#   end
-# end
+    ecto_book
+    |> cast(params, @fields)
+  end
+end
 
-# defmodule Bookshelf.Infrastructure.EctoBookRepo do
-#   @behaviour Bookshelf.Model.BookRepo
+defmodule Bookshelf.Infrastructure.EctoBookRepo do
+  @behaviour Bookshelf.Model.BookRepo
 
-#   alias Bookshelf.Infrastructure.EctoBook
-#   alias Bookshelf.Model.Book
-#   alias Bookshelf.Repo
+  alias Bookshelf.Infrastructure.EctoBook
+  alias Bookshelf.Model.Book
+  alias Bookshelf.Repo
 
-#   def save(book) do
-#     {:ok, ecto_book} =
-#       EctoBook
-#       |> Repo.get(book.id)
-#       |> case do
-#         nil -> %EctoBook{}
-#         ecto_book -> ecto_book
-#       end
-#       |> EctoBook.changeset(book)
-#       |> Repo.insert_or_update()
+  def save(book) do
+    {:ok, ecto_book} =
+      EctoBook
+      |> Repo.get(book.id)
+      |> case do
+        nil -> %EctoBook{}
+        ecto_book -> ecto_book
+      end
+      |> EctoBook.changeset(book)
+      |> Repo.insert_or_update()
 
-#     {:ok, Book.new(ecto_book)}
-#   end
+    {:ok, Book.new(ecto_book)}
+  end
 
-#   def by_id(id) do
-#     EctoBook
-#     |> Repo.get(id)
-#     |> Book.new()
-#   end
-# end
+  def by_id(id) do
+    EctoBook
+    |> Repo.get(id)
+    |> Book.new()
+  end
+end
